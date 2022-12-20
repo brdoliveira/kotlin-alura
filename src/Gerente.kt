068270ec00fdbf2ab5b326d1353884c2af83214c
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-19 23:44:12 -0300

    FEAT(Gerente):Criando Classe Gerente

--- src/Gerente.kt
+++ src/Gerente.kt
@@ -1,2 +1,17 @@
-class Gerente {
+class Gerente(
+    val nome: String,
+    val cpf: String,
+    val salario: Double,
+    val senha: Int
+) {
+    fun bonificacao(): Double{
+        return salario * 0.1
+    }
+
+    fun autentica(senha: Int): Boolean {
+        if(this.senha == senha){
+            return true
+        }
+        return false
+    }
 }
