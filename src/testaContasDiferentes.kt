e0fa4a1f6dc1e561eef615da7f51f93a05a7ba31
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-21 23:54:56 -0300

    FEAT(testaContasDiferentes): Criando testaContasDiferentes

---
+++ src/testaContasDiferentes.kt
@@ -0,0 +1,33 @@
+fun testaContasDiferentes() {
+    val contaCorrente = ContaCorrente(
+        titular = "Alex",
+        numero = 1000
+    )
+
+    val contaPoupanca = ContaPoupanca(
+        titular = "Fran",
+        numero = 1001
+    )
+
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
+
+    println("saldo conta corrente: ${contaCorrente.saldo}")
+    println("saldo conta poupanca: ${contaPoupanca.saldo}")
+
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    println("saldo após saque - conta corrente: ${contaCorrente.saldo}")
+    println("saldo após saque - conta poupanca: ${contaPoupanca.saldo}")
+
+    contaCorrente.transfere(100.0, contaPoupanca)
+
+    println("saldo conta corrente após transferir para conta poupança: ${contaCorrente.saldo}")
+    println("saldo conta poupanca após receber transferencia: ${contaPoupanca.saldo}")
+
+    contaPoupanca.transfere(100.0, contaCorrente)
+
+    println("saldo conta poupança após transferir para conta corrente: ${contaPoupanca.saldo}")
+    println("saldo conta corrente após receber transferencia: ${contaCorrente.saldo}")
+}
