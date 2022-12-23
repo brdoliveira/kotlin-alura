8a81ef0bfc8dedc0caec5c359c1a308dcb4ed095
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-22 23:50:47 -0300

    FEAT(Auxiliar): Criando classe Auxiliar

--- src/Auxiliar.kt
+++ src/Auxiliar.kt
@@ -1,2 +1,11 @@
-class Auxiliar {
+class Auxiliar(
+    nome : String,
+    cpf: String,
+    salario: Double
+) : Funcionario(
+    nome= nome,
+    cpf=cpf,
+    salario = salario
+) {
+    override val bonificacao: Double get() = salario * 0.05
 }
