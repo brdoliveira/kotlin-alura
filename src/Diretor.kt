f5cd756fdaba6c96a743de83f58a4689130119b2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-22 23:51:43 -0300

    FEAT(Diretor): Herdando da classe FuncionarioAdmin

--- src/Diretor.kt
+++ src/Diretor.kt
@@ -2,21 +2,13 @@ class Diretor(
     nome : String,
     cpf: String,
     salario: Double,
-    val senha : Int,
+    senha : Int,
     val plr: Double
-) : Funcionario(
+) : FuncionarioAdmin(
     nome= nome,
     cpf=cpf,
-    salario = salario
+    salario = salario,
+    senha = senha
 ) {
-
-    override val bonificacao get() = super.bonificacao + salario + plr
-
-
-    fun autentica(senha: Int): Boolean {
-        if(this.senha == senha){
-            return true
-        }
-        return false
-    }
+    override val bonificacao get() = salario + plr
 }
