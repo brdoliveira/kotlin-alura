49cb5bf5130ffb78a34a40193c65c6547ba2fcdd
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-22 23:51:04 -0300

    FEAT(Cliente): Criando classe Cliente

--- src/Cliente.kt
+++ src/Cliente.kt
@@ -1,2 +1,12 @@
-class Cliente {
+class Cliente(
+    val nome: String,
+    val cpf: String,
+    val senha: Int):
+Autenticavel {
+    override fun autentica(senha: Int): Boolean {
+        if(this.senha == senha){
+            return true
+        }
+        return false
+    }
 }
