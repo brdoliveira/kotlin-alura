50554234cce4088ff2f16811f6a831e3a667253b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-23 23:08:28 -0300

    FEAT(Cliente): Adicionando classe de Endereco

--- src/br/com/alura/bytebank/modelo/Cliente.kt
+++ src/br/com/alura/bytebank/modelo/Cliente.kt
@@ -1,8 +1,9 @@
 package br.com.alura.bytebank.modelo
 
 class Cliente(
-    val nome: String,
+    var nome: String,
     val cpf: String,
+    var endereco: Endereco = Endereco(),
     val senha: Int):
     Autenticavel {
     override fun autentica(senha: Int): Boolean {
