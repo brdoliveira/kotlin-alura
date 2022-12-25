ad9485316b6e6dcfa9d8c5b78a72432a9eae533c
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-25 20:17:12 -0300

    FEAT(main): Testando pilha

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,24 +1,55 @@
+package br.com.alura.bytebank
+
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    val endereco = Endereco(
-        logradouro = "Rua Vergueiro",
-        complemento = "Alura",
-        cep = "00000-000"
-    )
-    val enderecoNovo = Endereco(
-        bairro = "Vila Mariana",
-        numero = 1000,
-        cep= "00000-000"
-    )
+    println("início main")
+    val entrada: String = "1,9"
 
-    println(endereco.hashCode())
-    println(endereco.equals(enderecoNovo))
-    println(endereco.toString())
+    val valorRecebido: Double? = try{
+        entrada.toDouble()
+    }catch (e: NumberFormatException){
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
 
+    val valorComTaxa: Double? = when {
+        valorRecebido != null -> {
+            valorRecebido + 0.1
+        }
+        else -> {
+            null
+        }
+    }
+
+    if(valorComTaxa != null){
+        println("valor recebido: $valorComTaxa")
+    }else{
+        println("valor inválido")
+    }
+
+    funcao1()
+    println("fim main")
 }
 
-fun imprime(valor: Any): Any{
-    println(valor)
-    return valor
+fun funcao1(){
+    println("início funcao1")
+    try {
+        funcao2()
+    }catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastException foi pegada")
+    }
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5) {
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
 }
