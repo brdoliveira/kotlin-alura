c39e96527ea83fef5b8a204fa49f1bdf9dcac9c5
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-24 21:59:50 -0300

    FEAT(main): Criando teste com Any

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,1 +1,19 @@
-fun main() {}
+import br.com.alura.bytebank.modelo.Endereco
+
+fun main() {
+    val endereco = Endereco()
+    val objeto: Any = Any()
+
+    imprime(objeto)
+    imprime(Unit)
+    imprime(1)
+    imprime(1.0)
+
+    val teste: Any = imprime(endereco)
+    println(teste)
+}
+
+fun imprime(valor: Any): Any{
+    println(valor)
+    return valor
+}
