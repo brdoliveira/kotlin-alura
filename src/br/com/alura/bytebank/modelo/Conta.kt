a43034ec3ae9d03f7984d2592d9dc36959de88c8
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-26 03:01:24 -0300

    FEAT(Conta): Adicionando método autentica e validações

--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -1,9 +1,12 @@
 package br.com.alura.bytebank.modelo
 
+import br.com.alura.bytebank.exception.FalhaAutenticacaoException
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
+
 abstract class Conta(
     var titular: Cliente,
     val numero: Int
-) {
+) : Autenticavel {
     var saldo = 0.0
         protected set
 
@@ -16,6 +19,10 @@ abstract class Conta(
         total++
     }
 
+    override fun autentica(senha: Int): Boolean {
+        return titular.autentica(senha)
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) {
             this.saldo += valor
@@ -24,7 +31,17 @@ abstract class Conta(
 
     abstract fun saca(valor: Double)
 
-    fun transfere(valor: Double, destino: Conta): Boolean {
+    fun transfere(valor: Double, destino: Conta, senha: Int): Boolean {
+        if(saldo < valor){
+           throw SaldoInsuficienteException(
+               mensagem = "O saldo é insuficiente, saldo atual: $saldo, valor a ser subtraído $valor"
+           )
+        }
+
+        if(!autentica(senha)){
+            throw FalhaAutenticacaoException()
+        }
+
         if (saldo >= valor) {
             saldo -= valor
             destino.deposita(valor)
