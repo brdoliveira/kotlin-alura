736a31d415ce54dda4c7f46442a19d8d9ed6beed
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-26 02:55:30 -0300

    FEAT(testaExpressao): Criando teste testaExpressao

--- src/br/com/alura/bytebank/teste/testaExpressao.kt
+++ src/br/com/alura/bytebank/teste/testaExpressao.kt
@@ -1,2 +1,28 @@
 package br.com.alura.bytebank.teste
 
+fun testaExpressao() {
+    val entrada: String = "1,9"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = when {
+        valorRecebido != null -> {
+            valorRecebido + 0.1
+        }
+        else -> {
+            null
+        }
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+}
