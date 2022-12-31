5bbf7ccf6c27b5517f3d222c81ba82ebc20904ec
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-30 23:53:04 -0300

    FEAT(SistemaInterno): Criando melhoria na validação

---
+++ src/br/com/alura/bytebank/teste/testeNulable.kt
@@ -0,0 +1,23 @@
+package br.com.alura.bytebank.teste
+
+import br.com.alura.bytebank.modelo.Endereco
+
+fun testeNulo(){
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "Prédio")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
+    }
+
+    teste("")
+    teste(1)
+}
+
+fun teste(valor: Any){
+    val numero: Int? = valor as? Int
+    println(numero)
+}
