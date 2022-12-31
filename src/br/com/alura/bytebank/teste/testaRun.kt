7e862bc9e3df92b6740d46e38c44e547a5ea09b6
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-31 04:49:58 -0300

    FEAT(testaRun): Criando teste testaRun

--- src/br/com/alura/bytebank/teste/testaRun.kt
+++ src/br/com/alura/bytebank/teste/testaRun.kt
@@ -1,4 +1,27 @@
 package br.com.alura.bytebank.teste
 
-class testaRun {
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("Taxa mensal: $taxaMensal")
+
+    val contaPoupanca = ContaPoupanca(Cliente(nome = "Alex", cpf = "111.111.111-11", senha = 1234), 1000)
+    contaPoupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }.let { rendimentoMensal ->
+        println("Rendimento mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) { indice ->
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+    println("Rendimento anual (simulação): $rendimentoAnual")
 }
