47f971bd04ba7c4de069850582c56225d3191f50
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-12-31 04:49:25 -0300

    FEAT(testaFuncoesEscopo): Criando teste testaFuncoesEscopo

--- src/br/com/alura/bytebank/teste/testaFuncoesEscopo.kt
+++ src/br/com/alura/bytebank/teste/testaFuncoesEscopo.kt
@@ -1,2 +1,32 @@
 package br.com.alura.bytebank.teste
 
+import br.com.alura.bytebank.modelo.Endereco
+
+fun testaFuncoesEscopo() {
+    //    val endereco  = Endereco(logradouro = "Rua Vergueiro", numero = 3185)
+    //    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    //    println(enderecoEmMaiusculo)
+    var endereco = Endereco()
+        .also { println("Criando endereco") }
+        .apply {
+            logradouro = "Rua vergueiro"
+            numero = 3185
+        }
+
+    with(endereco) {
+        "$logradouro, $numero".toUpperCase()
+    }.let(::println)
+
+    Endereco(logradouro = "Rua Vergueiro", numero = 3185)
+        .run {
+            "$logradouro, $numero".toUpperCase()
+        }.let(::println)
+
+    listOf(
+        Endereco(complemento = "casa"),
+        Endereco(),
+        Endereco(complemento = "apartamento")
+    )
+        .filter(predicate = { endereco -> endereco.complemento.isNotEmpty() })
+        .let(block = ::println)
+}
